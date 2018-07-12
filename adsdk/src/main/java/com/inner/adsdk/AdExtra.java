f0e684ccc827e37c76565aefa6187d195c94d11f
weiliuzhao <liuzhao@chinawebeye.com>
2018-07-12 13:10:14 +0800

    1. 增加ad类型 2. 升级版本号

--- adsdk/src/main/java/com/inner/adsdk/AdExtra.java
+++ adsdk/src/main/java/com/inner/adsdk/AdExtra.java
@@ -168,4 +168,24 @@ public class AdExtra {
      * 原生广告模板值
      */
     public static final int NATIVE_CARD_LARGE = Constant.NATIVE_CARD_LARGE;
+
+    /**
+     * BANNER类型
+     */
+    public static final String AD_TYPE_BANNER = Constant.TYPE_BANNER;
+
+    /**
+     * INTERSTITIAL类型
+     */
+    public static final String AD_TYPE_INTERSTITIAL = Constant.TYPE_INTERSTITIAL;
+
+    /**
+     * NATIVE类型
+     */
+    public static final String AD_TYPE_NATIVE = Constant.TYPE_NATIVE;
+
+    /**
+     * REWARD类型
+     */
+    public static final String AD_TYPE_REWARD = Constant.TYPE_REWARD;
 }
