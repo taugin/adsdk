4578a19553db0aaaff3af3e8e6a10d49b9e1f603
weiliuzhao <liuzhao@chinawebeye.com>
2018-08-03 21:18:32 +0800

    1. 同步remote对象

--- adsdk/src/main/java/com/inner/adsdk/request/RemoteConfigRequest.java
+++ adsdk/src/main/java/com/inner/adsdk/request/RemoteConfigRequest.java
@@ -59,17 +59,19 @@ public class RemoteConfigRequest implements IDataRequest, OnCompleteListener {
     public void refresh() {
         ensureFirebase();
         if (mFirebaseRemoteConfig != null) {
-            long now = System.currentTimeMillis();
-            long last = Utils.getLong(mContext, Constant.PREF_REMOTE_CONFIG_REQUEST_TIME);
-            boolean needRequest = now - last > REFRESH_INTERVAL;
-            Log.v(Log.TAG, "adsdk refresh now : " + Constant.SDF_1.format(new Date(now)) + " , last : " + Constant.SDF_1.format(new Date(last)) + " , do : " + needRequest);
-            if (needRequest) {
-                try {
-                    mFirebaseRemoteConfig.fetch(CACHE_EXPIRETIME).addOnCompleteListener(this);
-                    Utils.putLong(mContext, Constant.PREF_REMOTE_CONFIG_REQUEST_TIME, System.currentTimeMillis());
-                    Log.v(Log.TAG, "adsdk refresh fetch called");
-                } catch (Exception e) {
-                    Log.e(Log.TAG, "error : " + e);
+            synchronized (mFirebaseRemoteConfig) {
+                long now = System.currentTimeMillis();
+                long last = Utils.getLong(mContext, Constant.PREF_REMOTE_CONFIG_REQUEST_TIME);
+                boolean needRequest = now - last > REFRESH_INTERVAL;
+                Log.v(Log.TAG, "adsdk refresh now : " + Constant.SDF_1.format(new Date(now)) + " , last : " + Constant.SDF_1.format(new Date(last)) + " , do : " + needRequest);
+                if (needRequest) {
+                    try {
+                        mFirebaseRemoteConfig.fetch(CACHE_EXPIRETIME).addOnCompleteListener(this);
+                        Utils.putLong(mContext, Constant.PREF_REMOTE_CONFIG_REQUEST_TIME, System.currentTimeMillis());
+                        Log.v(Log.TAG, "adsdk refresh fetch called");
+                    } catch (Exception e) {
+                        Log.e(Log.TAG, "error : " + e);
+                    }
                 }
             }
         }
