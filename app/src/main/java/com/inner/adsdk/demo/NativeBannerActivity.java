df12fa1b8a105299e9656945aa073b1d1f388e20
weiliuzhao <liuzhao@chinawebeye.com>
2018-10-31 18:44:14 +0800

    1. 临时提交

---
+++ app/src/main/java/com/inner/adsdk/demo/NativeBannerActivity.java
@@ -0,0 +1,25 @@
+package com.inner.adsdk.demo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.DisplayMetrics;
+import android.view.WindowManager;
+
+import com.appub.ads.a.FSA;
+
+/**
+ * Created by Administrator on 2018-10-31.
+ */
+
+public class NativeBannerActivity extends FSA {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WindowManager.LayoutParams params = getWindow().getAttributes();
+        DisplayMetrics dm = getResources().getDisplayMetrics();
+        // params.width = (int) (dm.widthPixels * 0.9f);
+        params.height = (int) (dm.heightPixels * 0.6f);
+        getWindow().setAttributes(params);
+        getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+    }
+}
