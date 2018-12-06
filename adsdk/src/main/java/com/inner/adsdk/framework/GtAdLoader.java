e5ad0129e8ec1adbd6c4b4ef1908249f5b67b2d9
weiliuzhao <liuzhao@chinawebeye.com>
2018-12-06 16:28:25 +0800

    1. nt增加dspmob的250 banner

--- adsdk/src/main/java/com/inner/adsdk/framework/GtAdLoader.java
+++ adsdk/src/main/java/com/inner/adsdk/framework/GtAdLoader.java
@@ -158,6 +158,7 @@ public class GtAdLoader {
         builder.setBannerSize(AdExtra.AD_SDK_DFP, AdExtra.DFP_MEDIUM_RECTANGLE);
         builder.setBannerSize(AdExtra.AD_SDK_FACEBOOK, AdExtra.FB_MEDIUM_RECTANGLE);
         builder.setBannerSize(AdExtra.AD_SDK_ADX, AdExtra.ADX_MEDIUM_RECTANGLE);
+        builder.setBannerSize(AdExtra.AD_SDK_DSPMOB, AdExtra.DSPMOB_MEDIUM_RECTANGLE);
 
         int layoutId[] = new int[] {R.layout.native_card_full, R.layout.native_card_mix};
 
