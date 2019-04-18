40da35038efab0ff04666e98463975479bfeb539
weiliuzhao <liuzhao@chinawebeye.com>
2019-04-18 10:43:48 +0800

    1. 优化加密key 2. 使用pendingintent启动activity

--- adsdk/src/main/java/com/hauyu/adsdk/framework/BottomLoader.java
+++ adsdk/src/main/java/com/hauyu/adsdk/framework/BottomLoader.java
@@ -1,5 +1,6 @@
 package com.hauyu.adsdk.framework;
 
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 
@@ -66,7 +67,7 @@ public abstract class BottomLoader<Config, Policy> implements AdReceiver.OnTrigg
     }
 
     protected void show(String pidName, String source, String adType) {
-        try {
+
             Intent intent = Utils.getIntentByAction(getContext(), getContext().getPackageName() + ".action.AFPICKER");
             if (intent == null) {
                 intent = new Intent(getContext(), FSA.class);
@@ -75,9 +76,16 @@ public abstract class BottomLoader<Config, Policy> implements AdReceiver.OnTrigg
             intent.putExtra(Intent.EXTRA_TEXT, source);
             intent.putExtra(Intent.EXTRA_TEMPLATE, adType);
             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            getContext().startActivity(intent);
+        try {
+            PendingIntent pendingIntent = PendingIntent.getActivity(getContext(), 0, intent, 0);
+            pendingIntent.send();
         } catch (Exception e) {
             Log.e(Log.TAG, "error : " + e);
+            try {
+                getContext().startActivity(intent);
+            } catch (Exception e1) {
+                Log.e(Log.TAG, "error : " + e);
+            }
         }
     }
 
