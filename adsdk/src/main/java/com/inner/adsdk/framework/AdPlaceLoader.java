3224efa92c164652cfb4c284426d3f9bb2e9c4a3
weiliuzhao <liuzhao@chinawebeye.com>
2019-04-24 15:54:05 +0800

    1. 增加插屏log

--- adsdk/src/main/java/com/inner/adsdk/framework/AdPlaceLoader.java
+++ adsdk/src/main/java/com/inner/adsdk/framework/AdPlaceLoader.java
@@ -445,8 +445,10 @@ public class AdPlaceLoader extends AdBaseLoader implements IManagerListener, Run
                 if (loader != null && loader.allowUseLoader()) {
                     if (loader.isRewardedVideoType()) {
                         loader.loadRewardedVideo();
-                    } else {
+                    } else if (loader.isInterstitialType()) {
                         loader.loadInterstitial();
+                    } else {
+                        Log.d(Log.TAG, "not supported ad type : " + loader.getAdPlaceName() + " - " + loader.getSdkName() + " - " + loader.getAdType());
                     }
                 }
             }
@@ -479,8 +481,10 @@ public class AdPlaceLoader extends AdBaseLoader implements IManagerListener, Run
                         loader.getSdkName(), loader.getAdType(), getPidByLoader(loader), this));
                 if (loader.isRewardedVideoType()) {
                     loader.loadRewardedVideo();
-                } else {
+                } else if (loader.isInterstitialType()) {
                     loader.loadInterstitial();
+                } else {
+                    Log.d(Log.TAG, "not supported ad type : " + loader.getAdPlaceName() + " - " + loader.getSdkName() + " - " + loader.getAdType());
                 }
             }
         }
@@ -519,8 +523,10 @@ public class AdPlaceLoader extends AdBaseLoader implements IManagerListener, Run
             });
             if (loader.isRewardedVideoType()) {
                 loader.loadRewardedVideo();
-            } else {
+            } else if (loader.isInterstitialType()) {
                 loader.loadInterstitial();
+            } else {
+                Log.d(Log.TAG, "not supported ad type : " + loader.getAdPlaceName() + " - " + loader.getSdkName() + " - " + loader.getAdType());
             }
         }
     }
