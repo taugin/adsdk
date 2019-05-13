2a07c35f39e52a8ffd78ad2e1bf5d3764333c74a
weiliuzhao <liuzhao@chinawebeye.com>
2019-05-13 10:50:48 +0800

    1. 去除默认场景间隔 2. 更新wesdk

--- adsdk/src/main/java/com/inner/adsdk/common/BaseConfig.java
+++ adsdk/src/main/java/com/inner/adsdk/common/BaseConfig.java
@@ -23,7 +23,7 @@ public abstract class BaseConfig {
     private boolean showBottomActivity = true;
     private String placeNameInt;
     private String placeNameAdv;
-    private long sceneInterval = 300000;
+    private long sceneInterval;
 
     public abstract String getName();
 
