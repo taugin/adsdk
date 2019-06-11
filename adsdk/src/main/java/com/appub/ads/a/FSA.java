ca24f2fec247c0bb8fc45fe7c5323dbe46be3710
weiliuzhao <liuzhao@chinawebeye.com>
2019-06-11 15:52:57 +0800

    1. 增加shimmer自定义view

--- adsdk/src/main/java/com/appub/ads/a/FSA.java
+++ adsdk/src/main/java/com/appub/ads/a/FSA.java
@@ -16,6 +16,8 @@ import android.graphics.Color;
 import android.graphics.LinearGradient;
 import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
 import android.graphics.RectF;
 import android.graphics.Shader;
@@ -46,6 +48,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewConfiguration;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.view.Window;
 import android.view.WindowManager;
 import android.view.animation.AccelerateDecelerateInterpolator;
@@ -384,6 +387,7 @@ public class FSA extends Activity {
 
     /**
      * 是否立即显示广告
+     *
      * @return
      */
     protected boolean isAutoShowAdView() {
@@ -392,6 +396,7 @@ public class FSA extends Activity {
 
     /**
      * 返回关闭按钮
+     *
      * @return
      */
     protected View getCloseView() {
@@ -400,6 +405,7 @@ public class FSA extends Activity {
 
     /**
      * 获取推广标记
+     *
      * @return
      */
     protected View getSponsoredView() {
@@ -481,7 +487,7 @@ public class FSA extends Activity {
      */
     private void showGAd() {
         if (!TextUtils.isEmpty(mPidName)) {
-            boolean shown = AdSdk.get(this).showComplexAdsWithResult(mPidName, null,  mSource, mAdType, null);
+            boolean shown = AdSdk.get(this).showComplexAdsWithResult(mPidName, null, mSource, mAdType, null);
             if (!shown) {
                 finishActivityWithDelay();
             }
@@ -1646,6 +1652,166 @@ public class FSA extends Activity {
 
         public interface OnScreenListener {
             void onScreenUnlocked();
+        }
+    }
+
+    public static class ShimmerView extends View {
+        private static final int ANIMATION_TIME = 2000;
+        private Shader mGradient = null;
+        private Matrix mGradientMatrix = null;
+        private Paint mPaint;
+        private float mCornerRadius = 0f;
+        private int mViewWidth = 0;
+        private int mViewHeight = 0;
+        private float mTranslateX = 0f;
+        private float mTranslateY = 0f;
+        private RectF rectF;
+        private ValueAnimator valueAnimator;
+        private boolean autoRun = true; //是否自动运行动画
+
+        public ShimmerView(Context context) {
+            super(context);
+            init(context, null);
+        }
+
+        public ShimmerView(Context context, @Nullable AttributeSet attrs) {
+            super(context, attrs);
+            init(context, attrs);
+        }
+
+        public ShimmerView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+            super(context, attrs, defStyleAttr);
+            init(context, attrs);
+        }
+
+        private void init(Context context, AttributeSet attrs) {
+            rectF = new RectF();
+            mPaint = new Paint();
+            initGradientAnimator();
+        }
+
+        @Override
+        protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+            rectF.set(0, 0, getWidth(), getHeight());
+        }
+
+        @Override
+        protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+            super.onSizeChanged(w, h, oldw, oldh);
+            if (mViewWidth == 0) {
+                mViewWidth = getWidth();
+                mViewHeight = getHeight();
+                if (mViewWidth > 0) {
+                    mGradient = new LinearGradient(0f, 0f, mViewWidth, mViewHeight,
+                            new int[]{0x00ffffff, 0x73ffffff, 0x00ffffff},
+                            new float[]{0.4f, 0.45f, 0.5f},
+                            Shader.TileMode.CLAMP);
+                    mPaint.setShader(mGradient);
+                    mPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.LIGHTEN));
+                    mGradientMatrix = new Matrix();
+                    mGradientMatrix.setTranslate((-2 * mViewWidth), mViewHeight);
+                    mGradient.setLocalMatrix(mGradientMatrix);
+                    rectF.set(0, 0, w, h);
+                }
+            }
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            super.onDraw(canvas);
+            if (valueAnimator != null && valueAnimator.isRunning() && mGradientMatrix != null) {
+                canvas.drawRoundRect(rectF, mCornerRadius, mCornerRadius, mPaint);
+            }
+        }
+
+        private void initGradientAnimator() {
+            valueAnimator = ValueAnimator.ofFloat(0f, 1f);
+            valueAnimator.setDuration(ANIMATION_TIME);
+            valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                @Override
+                public void onAnimationUpdate(ValueAnimator animation) {
+                    float v = (float) animation.getAnimatedValue();
+                    //❶ 改变每次动画的平移x、y值，范围是[-2mViewWidth, 2mViewWidth]
+                    mTranslateX = 4f * mViewWidth * v - mViewWidth * 2;
+                    mTranslateY = mViewHeight * v;
+                    //❷ 平移matrix, 设置平移量
+                    if (mGradientMatrix != null) {
+                        mGradientMatrix.setTranslate(mTranslateX, mTranslateY);
+                    }
+                    //❸ 设置线性变化的matrix
+                    if (mGradient != null) {
+                        mGradient.setLocalMatrix(mGradientMatrix);
+                    }
+                    //❹ 重绘
+                    invalidate();
+                }
+            });
+            if (autoRun && valueAnimator != null) {
+                valueAnimator.setRepeatCount(ValueAnimator.INFINITE);
+                getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+
+                    public void onGlobalLayout() {
+                        getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                        startAnimation();
+                    }
+                });
+            }
+        }
+
+        @Override
+        protected void onWindowVisibilityChanged(int visibility) {
+            super.onWindowVisibilityChanged(visibility);
+            if (visibility == View.VISIBLE) {
+                startAnimation();
+            } else {
+                stopAnimation();
+            }
+        }
+
+        @Override
+        protected void onVisibilityChanged(View changedView, int visibility) {
+            super.onVisibilityChanged(changedView, visibility);
+            if (visibility == View.VISIBLE) {
+                startAnimation();
+            } else {
+                stopAnimation();
+            }
+        }
+
+        @Override
+        public void onWindowFocusChanged(boolean hasWindowFocus) {
+            super.onWindowFocusChanged(hasWindowFocus);
+            if (!hasWindowFocus) {
+                stopAnimation();
+            } else {
+                startAnimation();
+            }
+        }
+
+        @Override
+        public void setVisibility(int visibility) {
+            super.setVisibility(visibility);
+            if (visibility == View.VISIBLE) {
+                startAnimation();
+            } else {
+                stopAnimation();
+            }
+        }
+
+        //停止动画
+        private void stopAnimation() {
+            if (valueAnimator != null && valueAnimator.isRunning()) {
+                valueAnimator.cancel();
+                invalidate();
+            }
+        }
+
+        //开始动画
+        private void startAnimation() {
+            if (valueAnimator != null && !valueAnimator.isRunning() && getVisibility() == View.VISIBLE) {
+                valueAnimator.start();
+            }
         }
     }
 }
