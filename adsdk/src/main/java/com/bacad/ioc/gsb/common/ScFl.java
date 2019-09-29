96905f931a7111cbc8e8e2ee053aa695fb9f8a80
weiliuzhao <liuzhao@chinawebeye.com>
2019-09-29 10:54:18 +0800

    1. 增加scfl文件

---
+++ adsdk/src/main/java/com/bacad/ioc/gsb/common/ScFl.java
@@ -0,0 +1,49 @@
+package com.bacad.ioc.gsb.common;
+
+/**
+ * Created by Administrator on 2018/5/5.
+ */
+
+public class ScFl {
+    public static final String SCFL_NAME = "scene_flag";
+
+    // 上报友盟
+    private boolean reportUmeng = true;
+    // 上报Firebase
+    private boolean reportFirebase = true;
+    // 上报Facebook
+    private boolean reportFacebook = true;
+
+    public boolean isReportUmeng() {
+        return reportUmeng;
+    }
+
+    public void setReportUmeng(boolean reportUmeng) {
+        this.reportUmeng = reportUmeng;
+    }
+
+    public boolean isReportFirebase() {
+        return reportFirebase;
+    }
+
+    public void setReportFirebase(boolean reportFirebase) {
+        this.reportFirebase = reportFirebase;
+    }
+
+    public boolean isReportFacebook() {
+        return reportFacebook;
+    }
+
+    public void setReportFacebook(boolean reportFacebook) {
+        this.reportFacebook = reportFacebook;
+    }
+
+    @Override
+    public String toString() {
+        return "ads{" +
+                ", ru=" + reportUmeng +
+                ", rf=" + reportFirebase +
+                ", rfb=" + reportFacebook +
+                '}';
+    }
+}
