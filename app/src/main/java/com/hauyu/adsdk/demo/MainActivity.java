de9c85bfaff3dd2380547cda4280c735bf86568e
weiliuzhao <liuzhao@chinawebeye.com>
2020-08-21 16:51:23 +0800

    1. 设置监听器

--- app/src/main/java/com/hauyu/adsdk/demo/MainActivity.java
+++ app/src/main/java/com/hauyu/adsdk/demo/MainActivity.java
@@ -302,6 +302,8 @@ public class MainActivity extends Activity {
     private void loadRewardIfNeed() {
         if (!AdSdk.get(this).isRewardedVideoLoaded("reward_video") && ActivityMonitor.get(this).appOnTop()) {
             AdSdk.get(this).loadRewardedVideo("reward_video", mRewardListener);
+        } else {
+            AdSdk.get(this).setOnAdSdkListener("reward_video", mRewardListener);
         }
     }
 
