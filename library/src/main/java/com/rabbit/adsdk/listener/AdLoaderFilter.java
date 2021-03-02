638b0db75a06204cd9db265c80cf6f6f4ea01e59
weiliuzhao <liuzhao@chinawebeye.com>
2021-03-02 13:09:06 +0800

    1. 增加AdLoaderFilter注释

--- library/src/main/java/com/rabbit/adsdk/listener/AdLoaderFilter.java
+++ library/src/main/java/com/rabbit/adsdk/listener/AdLoaderFilter.java
@@ -1,5 +1,8 @@
 package com.rabbit.adsdk.listener;
 
+/**
+ * 广告加载过滤器，返回true表示不加载此广告，返回false表示可以加载广告
+ */
 public interface AdLoaderFilter {
     boolean doFilter(String pidName, String sdk, String type);
 }
