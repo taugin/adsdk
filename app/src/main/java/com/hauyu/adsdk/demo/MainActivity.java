0b6d95c05781bdf8f02516d8a8028f3f06cd2cf5
weiliuzhao <liuzhao@chinawebeye.com>
2021-06-21 12:09:34 +0800

    1. 优化测试界面

--- app/src/main/java/com/hauyu/adsdk/demo/MainActivity.java
+++ app/src/main/java/com/hauyu/adsdk/demo/MainActivity.java
@@ -10,6 +10,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -64,6 +65,7 @@ public class MainActivity extends Activity implements CompoundButton.OnCheckedCh
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
         CheckBox switchView = findViewById(R.id.auto_reward);
         switchView.setOnCheckedChangeListener(this);
