216cd09e0544abc6f65a6957afe182104980f3b0
weiliuzhao <liuzhao@chinawebeye.com>
2022-06-17 11:46:20 +0800

    1. 升级tradplus7.9.0.01，升级admob21.0.0，升级smaato21.8.3

--- library/src/main/java/com/applovin/mediation/adapters/SmaatoMediationAdapter.java
+++ library/src/main/java/com/applovin/mediation/adapters/SmaatoMediationAdapter.java
@@ -134,7 +134,7 @@ public class SmaatoMediationAdapter
     public String getAdapterVersion()
     {
         // return com.applovin.mediation.adapters.smaato.BuildConfig.VERSION_NAME;
-        return "21.8.2.1";
+        return "21.8.3.0";
     }
 
     @Override
