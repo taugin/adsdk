52bdb3de79f98387d534ae777040d9bbf257d0f2
weiliuzhao <liuzhao@chinawebeye.com>
2022-09-27 17:24:29 +0800

    1. 更新applovin版本 2. 更新smaato版本

--- library/src/main/java/com/applovin/mediation/adapters/SmaatoMediationAdapter.java
+++ library/src/main/java/com/applovin/mediation/adapters/SmaatoMediationAdapter.java
@@ -134,7 +134,7 @@ public class SmaatoMediationAdapter
     public String getAdapterVersion()
     {
         // return com.applovin.mediation.adapters.smaato.BuildConfig.VERSION_NAME;
-        return "21.8.4.0";
+        return "21.8.5.0";
     }
 
     @Override
