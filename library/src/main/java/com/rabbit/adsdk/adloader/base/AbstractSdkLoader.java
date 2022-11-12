91c7cac82b18cada0df505547c0e01d32392144a
weiliuzhao <liuzhao@chinawebeye.com>
2022-11-12 09:27:23 +0800

    1. 代码优化

--- library/src/main/java/com/rabbit/adsdk/adloader/base/AbstractSdkLoader.java
+++ library/src/main/java/com/rabbit/adsdk/adloader/base/AbstractSdkLoader.java
@@ -891,7 +891,7 @@ public abstract class AbstractSdkLoader implements ISdkLoader, Handler.Callback 
             finalAdRevenue = getCpm() / 1000f;
         }
         mAdRevenue = finalAdRevenue;
-        Log.iv(Log.TAG, "network : " + network + " , type : " + getAdType() + " , value : " + mAdRevenue);
+        Log.iv(Log.TAG, "set revenue sdk : " + getSdkName() + " , network : " + network + " , type : " + getAdType() + " , value : " + mAdRevenue);
     }
 
     @Override
