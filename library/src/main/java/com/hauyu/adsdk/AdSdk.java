5a045248575f775685c20e6fbd6bbaa7afed3be6
taugin <taugin@163.com>
2023-06-15 16:41:31 +0800

    1. 检测是否初始化，抛出异常

--- library/src/main/java/com/hauyu/adsdk/AdSdk.java
+++ library/src/main/java/com/hauyu/adsdk/AdSdk.java
@@ -122,7 +122,12 @@ public class AdSdk {
     private void checkInitialize() {
         if (mInitialized == null || !mInitialized.get()) {
             try {
-                if (0 != (mContext.getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE)) {
+                boolean debuggable = false;
+                try {
+                    debuggable = 0 != (mContext.getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE);
+                } catch (Exception e) {
+                }
+                if (debuggable) {
                     throw new IllegalStateException("You must call AdSdk.get(context).init() first");
                 } else {
                     Log.iv(Log.TAG, "You must call AdSdk.get(context).init() first");
